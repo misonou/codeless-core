ea55a941c6cc6691725cacc5fff1247d7e977e1c
takerusilt <takeru.moe@gmail.com>
2017-01-05 22:08:22 +0800

    v1.1.371

--- src/net45/Codeless/EnumerableHelper.cs
+++ src/net45/Codeless/EnumerableHelper.cs
@@ -16,6 +16,7 @@ namespace Codeless {
     /// <returns>An enumerable which enumerates all descendant nodes of the specified node.</returns>
     [DebuggerStepThrough]
     public static IEnumerable<T> Descendants<T>(T source, Func<T, IEnumerable<T>> selector) {
+      CommonHelper.ConfirmNotNull(source, "source");
       CommonHelper.ConfirmNotNull(selector, "selector");
       foreach (T item in selector(source)) {
         yield return item;
@@ -34,12 +35,36 @@ namespace Codeless {
     /// <returns>An enumerable which enumerates all ancestor nodes of the specified node.</returns>
     [DebuggerStepThrough]
     public static IEnumerable<T> Ancestors<T>(T source, Func<T, T> selector) {
+      CommonHelper.ConfirmNotNull(source, "source");
+      CommonHelper.ConfirmNotNull(selector, "selector");
+      for (T current = selector(source); current != null; current = selector(current)) {
+        yield return current;
+      }
+    }
+
+    /// <summary>
+    /// Selects all ancestor objects including the input object in a tree-like data structure.
+    /// </summary>
+    /// <typeparam name="T">Type of objects to select.</typeparam>
+    /// <param name="source">An object representing a node in a tree-like data structure.</param>
+    /// <param name="selector">An delegate to select the arent node of a given node.</param>
+    /// <returns>An enumerable which enumerates all ancestor nodes of the specified node.</returns>
+    [DebuggerStepThrough]
+    public static IEnumerable<T> AncestorsAndSelf<T>(T source, Func<T, T> selector) {
+      CommonHelper.ConfirmNotNull(source, "source");
       CommonHelper.ConfirmNotNull(selector, "selector");
       for (T current = source; current != null; current = selector(current)) {
         yield return current;
       }
     }
-    
+
+    /// <summary>
+    /// Iterate through an enumerable and performs action on each element.
+    /// </summary>
+    /// <typeparam name="T">Type of elements to iterate through.</typeparam>
+    /// <param name="source">An enumerable which enumerates elements.</param>
+    /// <param name="action">A delegte to execute on each enumerated element.</param>
+    /// <returns></returns>
     [DebuggerStepThrough]
     public static IEnumerable<T> ForEach<T>(this IEnumerable<T> source, Action<T> action) {
       CommonHelper.ConfirmNotNull(source, "source");
