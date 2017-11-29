e2242ceb1f09a418af0f0fe733513550cac56648
Takeru Lee <takeru.moe@gmail.com>
2017-11-29 22:53:05 +0800

    Add caching to mime types

--- src/net35/Codeless/System.Net45/MimeMapping.cs
+++ src/net35/Codeless/System.Net45/MimeMapping.cs
@@ -2,6 +2,7 @@
 using Microsoft.Web.Administration;
 using Microsoft.Win32;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -13,6 +14,8 @@ namespace System.Web {
   /// Maps document extensions to content MIME types.
   /// </summary>
   public static class MimeMapping {
+    private static readonly ConcurrentFactory<string, Hashtable> cache = new ConcurrentFactory<string, Hashtable>();
+
     /// <summary>
     /// Returns the MIME mapping for the specified file name.
     /// </summary>
@@ -20,23 +23,37 @@ namespace System.Web {
     /// <returns></returns>
     public static string GetMimeMapping(string filename) {
       CommonHelper.ConfirmNotNull(filename, "filename");
+      if (HostingEnvironment.IsHosted) {
+        string siteName = HostingEnvironment.ApplicationHost.GetSiteName();
+        Hashtable ht = cache.GetInstance(siteName, LoadMimeMappings);
+        string value = (string)ht[Path.GetExtension(filename)];
+        if (value != null) {
+          return value;
+        }
+      }
       object entry = Registry.GetValue("HKEY_CLASSES_ROOT\\" + Path.GetExtension(filename), "Content Type", null);
       if (entry != null) {
         return entry.ToString();
       }
+      return "application/octet-stream";
+    }
+
+    private static Hashtable LoadMimeMappings(string arg) {
+      Hashtable ht = new Hashtable();
       using (HostingEnvironment.Impersonate()) {
         using (ServerManager serverManager = new ServerManager()) {
-          string siteName = HostingEnvironment.ApplicationHost.GetSiteName();
-          Microsoft.Web.Administration.Configuration config = serverManager.GetWebConfiguration(siteName);
+          Microsoft.Web.Administration.Configuration config = serverManager.GetWebConfiguration(arg);
           ConfigurationSection staticContentSection = config.GetSection("system.webServer/staticContent");
-          ConfigurationElementCollection staticContentCollection = staticContentSection.GetCollection();
-          ConfigurationElement mimeMap = staticContentCollection.FirstOrDefault(v => v.GetAttributeValue("fileExtension") != null && v.GetAttributeValue("fileExtension").ToString() == Path.GetExtension(filename));
-          if (mimeMap != null) {
-            return mimeMap.GetAttributeValue("mimeType").ToString().Split(';')[0];
+          foreach (ConfigurationElement elm in staticContentSection.GetCollection()) {
+            object attrExt = elm.GetAttributeValue("fileExtension");
+            object attrMimeType = elm.GetAttributeValue("mimeType");
+            if (attrExt != null && attrMimeType != null) {
+              ht[attrExt.ToString()] = attrMimeType.ToString().Split(';')[0];
+            }
           }
         }
       }
-      return "application/octet-stream";
+      return ht;
     }
   }
 }
